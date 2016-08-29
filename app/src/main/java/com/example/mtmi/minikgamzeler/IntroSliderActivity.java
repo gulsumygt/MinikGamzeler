6719b82348e21f6e01dc378aeefa05139ad0f8e1
gülsüm yiğit <glsm yiit>
2016-08-29 14:59:03 +0300

    add changes

--- app/src/main/java/com/example/mtmi/minikgamzeler/IntroSliderActivity.java
+++ app/src/main/java/com/example/mtmi/minikgamzeler/IntroSliderActivity.java
@@ -43,9 +43,9 @@ public class IntroSliderActivity extends AppCompatActivity {
             finish();
         }
 
-//        if(Build.VERSION.SDK_INT>=21){
-//            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-//        }
+        if(Build.VERSION.SDK_INT>=21){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
         setContentView(R.layout.activity_intro_slider);
 
 
