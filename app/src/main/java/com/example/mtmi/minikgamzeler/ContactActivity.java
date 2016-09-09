557db0fa8342735b39b1895602619a76c2fe20e0
tutkurock <harunkaygisiz91@gmail.com>
2016-09-09 14:58:28 +0300

    contactactivity is done

--- app/src/main/java/com/example/mtmi/minikgamzeler/ContactActivity.java
+++ app/src/main/java/com/example/mtmi/minikgamzeler/ContactActivity.java
@@ -7,14 +7,12 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class ContactActivity extends AppCompatActivity {
 
-    EditText ad_soyad,iletişimemail,konu,mesajınız;
-    Button  gönder;
-    ProgressBar progressBar;
+    EditText ad_soyad, konu, mesajınız;
+    Button gönder;
 
 
     @Override
@@ -23,30 +21,25 @@ public class ContactActivity extends AppCompatActivity {
         setContentView(R.layout.contact_activity);
 
         ad_soyad = (EditText) findViewById(R.id.Ad_Soyad);
-        iletişimemail= (EditText) findViewById(R.id.iletisim_email);
-        konu= (EditText) findViewById(R.id.konu);
-        mesajınız= (EditText) findViewById(R.id.Mesajınız);
-        gönder= (Button) findViewById(R.id.Gönder);
+        konu = (EditText) findViewById(R.id.konu);
+        mesajınız = (EditText) findViewById(R.id.Mesajınız);
+        gönder = (Button) findViewById(R.id.Gönder);
 
 
         gönder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
                 String adsoyadinput = ad_soyad.getText().toString();
-                String iletişimemailinput = iletişimemail.getText().toString();
                 String konuinput = konu.getText().toString();
                 String mesajınızinput = mesajınız.getText().toString();
-                showOrder(adsoyadinput, iletişimemailinput, konuinput, mesajınızinput);
+
 
                 if (TextUtils.isEmpty(adsoyadinput)) {
                     Toast.makeText(getApplicationContext(), "Lütfen Adınız ve SoyAdınızı giriniz!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if (TextUtils.isEmpty(iletişimemailinput)) {
-                    Toast.makeText(getApplicationContext(), "Lütfen Email adresinizi giriniz!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+
                 if (TextUtils.isEmpty(konuinput)) {
                     Toast.makeText(getApplicationContext(), "Lütfen Konu giriniz!", Toast.LENGTH_SHORT).show();
                     return;
@@ -56,28 +49,25 @@ public class ContactActivity extends AppCompatActivity {
                     return;
                 }
 
-                private void showOrder (String adsoyadinput, String iletişimemailinput, String konuinput,String mesajınızinput) {
-                    String result = adsoyadinput + iletişimemailinput + konuinput + mesajınızinput ;
+                String result = mesajınızinput + "\n" + "\n" + "\n" + adsoyadinput;
+                Intent email = new Intent(Intent.ACTION_SEND);
 
-                    //Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{"harun140591@hotmail.com"});
+                email.putExtra(Intent.EXTRA_SUBJECT, konuinput);
+                email.putExtra(Intent.EXTRA_TEXT, result);
 
-                    String emailAddress = "harun140591@hotmail.com";
 
-                    Intent intent = new Intent(Intent.ACTION_SEND);
-                    intent.setType("plain/text");
-                    intent.putExtra(Intent.EXTRA_EMAIL, emailAddress);
-                    intent.putExtra(Intent.EXTRA_TEXT, result);
+                //need this to prompts email client only
+                email.setType("message/rfc822");
 
-                }
+                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+
+
             }
+
 
         });
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (progressBar != null) {
-            progressBar.setVisibility(View.GONE);
-        }
-    }
+
+
 }
